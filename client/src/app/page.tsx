fcd2a792699eade5b991d2a3a0375673873e8bd4
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-11-15 09:21:14 -0400

    frontend/config: agrega react/react-in-jsx/scope:off para evitar la importacion explicita en de React en todos los archivos jsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
+		</div>
+	)
 }
