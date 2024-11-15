3ff2a5a7c11270f7c67e95db127614b1147e6e8c
Matias Paredes <tutecapo1985@gmail.com>
2024-11-15 00:07:49 -0300

    feat: initial commit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
+		</div>
+	)
 }
