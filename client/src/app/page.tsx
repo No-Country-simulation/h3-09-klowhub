edf2006b6079868e10a18f3c342170d6f5bcec52
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-11-16 13:30:28 -0400

    frontend/feature/etiquetas-pro-y-pais---comprador: agrega componente ProTag

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,7 @@
-import CategoryTag from '@/components/buyerTags/CategoryTag'
-
 export default function Home() {
 	return (
 		<div>
 			<h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
-			<CategoryTag>CRM</CategoryTag>
 		</div>
 	)
 }
---
+++ client/src/components/buyerTags/ProTag.tsx
@@ -0,0 +1,7 @@
+export default function ProTag() {
+	return (
+		<article className="text-xs font-medium text-white h-7 w-[45px] leading-7 text-center rounded-lg bg-gradient-to-r from-[#58759D] to-primary-a-500">
+			PRO
+		</article>
+	)
+}
