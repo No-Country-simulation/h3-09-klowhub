6c7fbbfbae54352bc27e83407b3a83704c9649ea
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-11-19 23:52:42 -0400

    frontend/feature/etiquetas: Agrega componentes de etiquetas en folder appTags

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,23 +1,7 @@
-import ConsultantCard from '@/components/cards/ConsultantCard'
-import { Consultant } from '@/models/consultant.model'
-
 export default function Home() {
-	const consultant: Consultant = {
-		id: '1',
-		name: 'Marcos Perez Donoso',
-		countryCode: 'bo',
-		technologies: ['appsheet'],
-		image: '/img/profile_test.jpeg',
-		sessions: 10,
-		reviews: 5,
-		languages: ['English', 'Spanish'],
-		price: 100
-	}
-
 	return (
 		<div>
 			<h1 className="text-3xl">WELCOME TO KLOWHUB</h1>
-			<ConsultantCard consultant={consultant} />
 		</div>
 	)
 }
