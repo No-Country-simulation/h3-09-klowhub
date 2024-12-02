68e48a826dd924f1a2913be044716986bcbfefbc
isakidev <gaspar.c.developer@gmail.com>
2024-12-01 21:43:58 -0300

    fix: rename import module

--- server/klowhub-gateway/src/app.module.ts
+++ server/klowhub-gateway/src/app.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 
-import { UsersModule } from './users/users.module';
+import { UserMsModule } from './users-ms/users-ms.module';
 import { StripeModule } from './stripe/stripe.module';
 import { raw } from 'express';
 
@@ -8,7 +8,7 @@ import { AppsModule } from './apps/apps.module';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [UsersModule, AppsModule, CoursesModule, StripeModule],
+  imports: [UserMsModule, AppsModule, CoursesModule, StripeModule],
   controllers: [],
   providers: [],
 })
