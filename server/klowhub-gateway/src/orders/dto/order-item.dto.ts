a95017a6d8ff777c08a635c9efa31d9df1807c8e
isakidev <gaspar.c.developer@gmail.com>
2024-12-14 19:34:14 -0300

    feat: add type in dto

--- server/klowhub-gateway/src/orders/dto/order-item.dto.ts
+++ server/klowhub-gateway/src/orders/dto/order-item.dto.ts
@@ -1,4 +1,9 @@
-import { IsNumber, IsPositive, IsUUID } from 'class-validator';
+import { IsEnum, IsNumber, IsPositive, IsUUID } from 'class-validator';
+
+enum PRODUCT_TYPE {
+  COURSE = 'COURSE',
+  APP = 'APP'
+}
 
 export class OrderItemDto {
   @IsUUID()
@@ -11,4 +16,9 @@ export class OrderItemDto {
   @IsNumber()
   @IsPositive()
   price: number;
+
+  @IsEnum(PRODUCT_TYPE, {
+    message: `Type must be "${Object.values(PRODUCT_TYPE).join(' - ')}"`
+  })
+  type: PRODUCT_TYPE
 }
