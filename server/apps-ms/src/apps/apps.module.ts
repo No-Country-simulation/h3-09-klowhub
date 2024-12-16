e5a0b5c807322e3d5da87690e610c680caf88808
Naza <omarnlujan@gmail.com>
2024-12-16 19:40:13 -0300

    feature: creator verification

--- server/apps-ms/src/apps/apps.module.ts
+++ server/apps-ms/src/apps/apps.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppsService } from './apps.service';
 import { AppsController } from './apps.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [],
   controllers: [AppsController],
-  providers: [AppsService],
+  providers: [AppsService, UserService],
 })
 export class AppsModule {}
